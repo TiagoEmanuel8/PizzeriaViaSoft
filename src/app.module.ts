5a63509d361e22479295aff184af2102e813c099
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2024-03-14 12:02:01 -0300

    chore: config prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { DataModule } from './modules/data.module';
 
 @Module({
   imports: [
+    DataModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
