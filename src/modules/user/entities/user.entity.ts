0dd22aab289cefc6ffa065c97785c4e5f5ef3e26
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2024-03-17 12:08:05 -0300

    feat: criando resolver user

--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -1,7 +1,38 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Field, ObjectType, ID } from '@nestjs/graphql';
+import { OrderEntity } from '../../orders/entities/order.entity';
 
 @ObjectType()
-export class User {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+export class UserEntity {
+  @Field(() => ID)
+  id: number;
+
+  @Field()
+  name: string;
+
+  @Field()
+  email: string;
+
+  @Field()
+  cpf: string;
+
+  @Field()
+  password: string;
+
+  @Field()
+  mobileNumber: string;
+
+  @Field()
+  address: string;
+
+  @Field()
+  district: string;
+
+  @Field()
+  city: string;
+
+  @Field()
+  cep: string;
+
+  @Field(() => [OrderEntity], { nullable: 'itemsAndList' })
+  orders?: OrderEntity[];
 }
