6804f411873fa4b9708a8b26c66ac15cb042e3f4
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2024-03-17 23:25:13 -0300

    feat: criptografando a senha

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -3,8 +3,15 @@ import { UserService } from './user.service';
 import { UserResolver } from './user.resolver';
 import { PrismaService } from 'src/infra/client/prisma.service';
 import { UserRepository } from './repository/impl/user.repository';
+import { PasswordHashService } from '../../shared/utilities/password-hash.utility';
 
 @Module({
-  providers: [UserResolver, UserService, PrismaService, UserRepository],
+  providers: [
+    UserResolver,
+    UserService,
+    PrismaService,
+    UserRepository,
+    PasswordHashService,
+  ],
 })
 export class UserModule {}
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -3,13 +3,28 @@ import { CreateUserDto } from './dto/create-user.input';
 import { UpdateUserDto } from './dto/update-user.input';
 import { UserRepository } from './repository/impl/user.repository';
 import { UserEntity } from './entities/user.entity';
+import { PasswordHashService } from '../../shared/utilities/password-hash.utility';
 
 @Injectable()
 export class UserService {
-  constructor(private readonly repository: UserRepository) {}
+  constructor(
+    private readonly repository: UserRepository,
+    private readonly passwordHashService: PasswordHashService,
+  ) {}
 
   async create(createUser: CreateUserDto): Promise<UserEntity> {
-    return await this.repository.create(createUser);
+    const { password } = createUser;
+
+    const hashedPassword = await this.passwordHashService.hashPassword(
+      password,
+    );
+
+    const userWithHashedPassword = {
+      ...createUser,
+      password: hashedPassword,
+    };
+
+    return await this.repository.create(userWithHashedPassword);
   }
 
   async findAll() {
