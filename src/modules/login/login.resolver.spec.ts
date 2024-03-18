6fa38a4a0153e6e5944f3a30293a4d487721c375
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2024-03-18 19:51:54 -0300

    test: testando fluxo login

--- src/modules/login/login.resolver.spec.ts
+++ src/modules/login/login.resolver.spec.ts
@@ -1,9 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { LoginResolver } from './login.resolver';
 import { LoginService } from './login.service';
+import { LoginDto } from './dto/create-login.input';
+
+jest.mock('./login.service', () => {
+  return {
+    LoginService: jest.fn().mockImplementation(() => ({
+      create: jest.fn(),
+      findAll: jest.fn(),
+    })),
+  };
+});
 
 describe('LoginResolver', () => {
   let resolver: LoginResolver;
+  let loginService: LoginService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -11,9 +22,27 @@ describe('LoginResolver', () => {
     }).compile();
 
     resolver = module.get<LoginResolver>(LoginResolver);
+    loginService = module.get<LoginService>(LoginService);
   });
 
   it('should be defined', () => {
     expect(resolver).toBeDefined();
+  });
+
+  describe('createLogin', () => {
+    it('deve chamar LoginService.create com os parâmetros corretos', async () => {
+      const createLoginInput: LoginDto = {
+        email: 'test@example.com',
+        password: 'password123',
+      };
+      const expectedToken = 'jwtToken';
+
+      (loginService.create as jest.Mock).mockResolvedValue(expectedToken);
+
+      const result = await resolver.createLogin(createLoginInput);
+
+      expect(loginService.create).toHaveBeenCalledWith(createLoginInput);
+      expect(result).toEqual(expectedToken);
+    });
   });
 });
--- src/modules/login/login.service.spec.ts
+++ src/modules/login/login.service.spec.ts
@@ -1,18 +1,86 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { LoginService } from './login.service';
+import { LoginRepository } from './repository/impl/login.repository';
+import { PasswordHashService } from '../../shared/utilities/password-hash.utility';
+import { JwtService } from '@nestjs/jwt';
+import { LoginDto } from './dto/create-login.input';
+import { ApolloError } from 'apollo-server-express';
+
+const mockLoginRepository = {
+  verifyExisteField: jest.fn(),
+  findAll: jest.fn(),
+};
+
+const mockPasswordHashService = {
+  comparePasswords: jest.fn(),
+};
+
+const mockJwtService = {
+  sign: jest.fn(),
+};
 
 describe('LoginService', () => {
   let service: LoginService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [LoginService],
+      providers: [
+        LoginService,
+        { provide: LoginRepository, useValue: mockLoginRepository },
+        { provide: PasswordHashService, useValue: mockPasswordHashService },
+        { provide: JwtService, useValue: mockJwtService },
+      ],
     }).compile();
 
     service = module.get<LoginService>(LoginService);
+
+    mockLoginRepository.verifyExisteField.mockClear();
+    mockPasswordHashService.comparePasswords.mockClear();
+    mockJwtService.sign.mockClear();
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('deve gerar um erro se email ou a senha estiverem incorretos', async () => {
+      const loginDto: LoginDto = {
+        email: 'test@example.com',
+        password: 'test123',
+      };
+      mockLoginRepository.verifyExisteField.mockResolvedValue(null);
+
+      await expect(service.create(loginDto)).rejects.toThrow(ApolloError);
+    });
+
+    it('deve retornar um token JWT para credenciais válidas', async () => {
+      const loginDto: LoginDto = {
+        email: 'test@example.com',
+        password: 'test123',
+      };
+      const mockUser = {
+        id: 1,
+        email: 'test@example.com',
+        password: 'hashedPassword',
+      };
+      const expectedToken = 'jwtToken';
+
+      mockLoginRepository.verifyExisteField.mockResolvedValue(mockUser);
+      mockPasswordHashService.comparePasswords.mockResolvedValue(true);
+      mockJwtService.sign.mockReturnValue(expectedToken);
+
+      const result = await service.create(loginDto);
+
+      expect(mockLoginRepository.verifyExisteField).toHaveBeenCalledWith(
+        loginDto.email,
+      );
+      expect(mockPasswordHashService.comparePasswords).toHaveBeenCalledWith(
+        loginDto.password,
+        mockUser.password,
+      );
+      expect(mockJwtService.sign).toHaveBeenCalledWith({ id: mockUser.id });
+      expect(result).toEqual(expectedToken);
+    });
   });
 });
