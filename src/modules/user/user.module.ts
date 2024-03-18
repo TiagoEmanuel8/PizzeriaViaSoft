186b33a125ee9c8511acefe9a765e5fe5092f1df
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2024-03-18 12:38:53 -0300

    fix: disponibilizando jwt modulo users

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -4,6 +4,8 @@ import { UserResolver } from './user.resolver';
 import { PrismaService } from 'src/infra/client/prisma.service';
 import { UserRepository } from './repository/impl/user.repository';
 import { PasswordHashService } from '../../shared/utilities/password-hash.utility';
+import { JwtAuthGuard } from '../../shared/guards/jwt-auth.guard';
+import { JwtStrategy } from '../../shared/auth/jwt.strategy';
 
 @Module({
   providers: [
@@ -12,6 +14,8 @@ import { PasswordHashService } from '../../shared/utilities/password-hash.utilit
     PrismaService,
     UserRepository,
     PasswordHashService,
+    JwtAuthGuard,
+    JwtStrategy,
   ],
 })
 export class UserModule {}
